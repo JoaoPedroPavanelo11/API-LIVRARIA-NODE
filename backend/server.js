573c9d899155c95d511934b76ab8c5dddb3efcdc
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-07 22:42:39 -0300

    criação da porta e rotas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 import http from "http";
 
+const PORT = 3000;
+//Criei uma const porta que será a porta de acesso
+
+const rotas = {
+    "/": "Curso de NodeJS",
+};
+
 // Criando o servidor HTTP , acima eu importei o modulo http que o node tem
 const server = http.createServer((req , res) => {
     res.writeHead(200, { "Content-type": "text/plain"});
     res.end("Curso de NodeJS");
 });
 
+server.listen(PORT, () => {
+    console.log("Servidor rodando ");
+});
