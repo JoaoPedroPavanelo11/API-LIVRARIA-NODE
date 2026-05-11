2da34fffe4af58f30e68d4e46f0b1ae8ba23227d
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-10 21:14:05 -0300

    Criação do metodo POST para adicionar livros , e a criação da rota para verificar um livro especifico

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express from 'express';
 //comando para importar o express que foi instalado no cmd
 
 const app = express();
+app.use(express.json());
 
 const livros = [
     {
@@ -14,6 +15,13 @@ const livros = [
     }
 ]
 
+//Função responsavel por buscar um livro especifico, ela recebe o id do livro como parametro e retorna o livro correspondente
+function buscadorLivros(id) {
+    return livros.findIndex(livros => {
+        return livros.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJs");
 });
@@ -21,6 +29,19 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
+});
+
+//Rota para acessar um livro especifico , isso acontece via ID(EX: livro/1)
+app.get("/livros/:id", (req, res) => {
+    const index = buscadorLivros(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+//rota para criar um POST
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado!");
+    //codigo 201 é que foi criado algo novo, nesse caso um livro novo
 });
 
 export default app;
