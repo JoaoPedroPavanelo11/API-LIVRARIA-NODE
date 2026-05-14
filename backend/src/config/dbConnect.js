ed50cdc11ed78d54ecdc699b1ce5237f2c40c438
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-14 11:24:43 -0300

    Criação da função assincrona de conexao do mongodb , e a exportação

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,4 +1,11 @@
 import mongoose from 'mongoose';
 
-//String de conexção do MongoDB, substitua <db_password> pela senha do seu usuário do MongoDB Atlas.
-mongoose.connect("mongodb+srv://joaopedropavanelo_db_user:jpp260208@cluster0.4ubbw9k.mongodb.net/?appName=Cluster0");
+
+async function connectDB(){
+    //String de conexção do MongoDB, substitua <db_password> pela senha do seu usuário do MongoDB Atlas.
+    mongoose.connect("mongodb+srv://joaopedropavanelo_db_user:jpp260208@cluster0.4ubbw9k.mongodb.net/livraria?appName=Cluster0");
+    
+    return mongoose.connection;
+};
+
+export default connectDB;
