492ae0ad0bc26b87981a038d1d3519c17a6e8eff
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-14 18:03:54 -0300

    Criacção de um Schema , separando em id , titulo , editora , preço e paginas e por fim exportando esse schema

---
+++ backend/src/models/livros.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+//Schema do livro, onde serão definidos os campos e seus tipos
+const livrosSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    titulo: {type: mongoose.Schema.Types.String, required: true},
+    editora: {type: mongoose.Schema.Types.String},
+    preco: {type: mongoose.Schema.Types.Number},
+    paginas: {type: mongoose.Schema.Types.Number},
+}, {versionKey: false});
+
+const livro = mongoose.model("livros", livrosSchema);
+
+export default livro;
