3e20a8c0eb4a3593e78bcfef225f455c3a6aa0a5
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-14 12:03:24 -0300

    Criação da models

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -5,7 +5,7 @@ dns.setServers(["1.1.1.1","8.8.8.8"])
 
 async function connectDB(){
     //String de conexção do MongoDB, substitua <db_password> pela senha do seu usuário do MongoDB Atlas.
-    mongoose.connect("mongodb+srv://joaopedropavanelo_db_user:jpp260208@cluster0.4ubbw9k.mongodb.net/livros?appName=Cluster0");
+    mongoose.connect("mongodb+srv://joaopedropavanelo_db_user:jpp260208@cluster0.4ubbw9k.mongodb.net/livraria?appName=Cluster0");
     
     return mongoose.connection;
 };
